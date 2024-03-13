ec9e70e336b2b20e7f66e8ef6f6bc0757f6ff32c
elimendo <emendoza8361@gmail.com>
2024-03-13 15:30:35 -0700

    progression in the works i havent solved schema introspection failure but i wont olet that get me down i will prevail

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,46 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  input BookInput {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+   me: User 
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(bookData: BookInput): User 
+    removeBook(bookId: String): User
+    
+  }
+`;
+
+module.exports = typeDefs;
